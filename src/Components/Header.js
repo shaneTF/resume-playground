46d7b5d96d1e9a8ee1926df868cfe2468a313a67
Shane Franklin <shanefranklin148@gmail.com>
2022-05-26 15:29:44 -0400

    Added profile info, updated profile pic, removed portfolio, added resume download link.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -64,9 +64,9 @@ class Header extends Component {
 
         <div className="row banner">
           <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
+            <h1 className="responsive-headline">{name}.</h1>
             <h3>
-              I'm an {city} based <span>{occupation}</span>. {description}.
+              I'm a <span>{occupation}</span> based out of {city}. {description}.
             </h3>
             <hr />
             <ul className="social">{networks}</ul>
